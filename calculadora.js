bd1043274b5795668f9870e698ac1f9eab353e56
reinaldo2727 <reinaldopanuchi2727@gmail.com>
2022-01-10 13:39:45 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function insert(numero){
+    var num = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = num + numero
+}
+
+function clean(){
+    document.getElementById('resultado').innerHTML = ""
+}
+
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1)
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }else{
+        document.getElementById('resultado').innerHTML = 'Nada.....'
+    }
+}
